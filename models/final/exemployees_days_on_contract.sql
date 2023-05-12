49558e2f6418190d3f4a3e1bef9e695cda21ab79
hh5@nimbusintelligence.com <hh5@nimbusintelligence.com>
2023-05-12 11:12:35 +0000

    created employee tables except the day counting stuff

---
+++ models/final/exemployees_days_on_contract.sql
@@ -0,0 +1,26 @@
+with
+
+-- imports
+exemployees as (
+    select * from {{ ref('emp__exemployees') }}
+),
+
+employee_info as (
+    select * from {{ ref('emp__employee_info') }}
+),
+
+-- operations
+exemployees_days_on_contract as (
+    select
+        exemployees.id,
+        exemployees.team,
+        employee_info.first_name,
+        employee_info.last_name,
+        exemployees.end_date - exemployees.start_date as service_days
+    from exemployees
+    left join employee_info
+        on exemployees.id = employee_info.id
+)
+
+-- final
+select * from exemployees_days_on_contract
