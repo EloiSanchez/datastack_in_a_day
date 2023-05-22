fe660f4aa760d7ede190125c85ea9045cce6576d
hh5@nimbusintelligence.com <hh5@nimbusintelligence.com>
2023-05-22 12:29:06 +0000

    masking policy to exemployees

--- models/intermediate/emp__exemployees.sql
+++ models/intermediate/emp__exemployees.sql
@@ -2,7 +2,9 @@
     config(
         post_hook=[
                 "{{ create_rla_employees(model.database, model.schema) }}",
-                "alter view {{ this }} add row access policy {{ model.database }}.{{ model.schema }}.rla_employees on (team)"
+                "alter view {{ this }} add row access policy {{ model.database }}.{{ model.schema }}.rla_employees on (team)",
+                "{{ dbt_snow_mask.create_masking_policy('models') }}",
+                "{{ dbt_snow_mask.apply_masking_policy('models') }}"
             ]
     )
 }}
